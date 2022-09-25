be3aa228c13d8356e93731151bec34061eb8cf77
Give <papanda.5656@outlook.com>
2022-09-25 23:33:59 +0900

    fix: all_dates内の処理内容を統合&変更

--- test.rb
+++ test.rb
@@ -1,13 +1,4 @@
-require 'optparse'
 require 'date'
-
-opt = OptParse.new
-
-def create_top_bar(month)
-  this_year = Date.today.year
-  puts "      #{month}月 #{this_year}      "
-  puts "日 月 火 水 木 金 土"
-end
 
 def all_dates(month)
   this_year = Date.today.year
@@ -34,18 +25,4 @@ def all_dates(month)
   print ("\n")
 end
 
-opt.on('-m [VAL]') {|m| 
-  if m.to_i >= 1 && m.to_i <= 12
-    month = m.to_i
-    create_top_bar(month)
-    all_dates(month)
-  elsif m.nil?
-    month = Date.today.month
-    create_top_bar(month)
-    all_dates(month)
-  else
-    puts "#{m} is neither a month number (1..12) nor a name"
-  end
-}          # <- [VAL] を追加
-
-opt.parse!(ARGV)
+all_dates(9)
