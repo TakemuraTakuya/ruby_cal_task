5eac8af21d88eaa6e969b2342fb19b66bd8fb660
Give <papanda.5656@outlook.com>
2022-09-29 22:25:00 +0900

    fix: all_datesメソッドのif文を省略

--- calendar.rb
+++ calendar.rb
@@ -14,22 +14,15 @@ def all_dates(month)
   first_day = Date.new(this_year, month, 1)
   last_day = Date.new(this_year, month, -1)
   
+  space = "   "
+  print space * first_day.wday
   date = 1
   while date <= last_day.day
-    if date == first_day.day
-      space = "   "
-      print space * first_day.wday, date.to_s.rjust(2)," "
-      print("\n") if first_day.wday == 6
-    else
-      printf("%2d",date)
-      print " "
-  
-      next_date = Date.new(this_year, month, date)
-      print("\n") if next_date.wday == 6
-    end
-
+    printf("%2d",date)
+    print " "
+    next_date = Date.new(this_year,month,date)
+    print("\n") if next_date.wday == 6
     date += 1
-
   end
   print ("\n")
 end
